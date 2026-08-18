40ba58c8e466e18f01a0c536a6c11c7e7270fdd7
David Wadström <davidwadimp@gmail.com>
2026-08-18 09:42:54 +0000

    first try to generate puzzle

--- backend/sudoku.py
+++ backend/sudoku.py
@@ -1,6 +1,9 @@
 import random
+import copy
 
 def check(row, col, digit, board):
+	if digit == 0:
+		return True
 	for i in range(9):
 		if col != i and board[row][i] == digit:
 			return False
@@ -46,7 +49,59 @@ def gen_board():
 	gen_from(0, 0, board)
 	return board
 
+def solve_from(row, col, board):
+	if col == 9:
+		col = 0
+		row += 1
+		if row == 9:
+			return 1
+	
+	if board[row][col] != 0:
+		return solve_from(row, col + 1, board)
+	
+	solutions = 0
+	for digit in valid_at(row, col, board):
+		board[row][col] = digit
+		
+		solutions += solve_from(row, col + 1, board)
+		
+		board[row][col] = 0
+	return solutions
+
+def solve(board):
+	return solve_from(0, 0, board)
+
+def gen_puzzle(board, difficulty='medium'):
+	targets = {
+		'easy': 41,
+		'medium': 49,
+		'hard': 55
+	}
+	
+	target 	= targets[difficulty]
+	removed = 0
+	
+	positions = [(r, c) for r in range(9) for c in range(9)]
+	random.shuffle(positions)
+	
+	for row, col in positions:
+		if removed == target:
+			break
+		
+		temp = board[row][col]
+		
+		board_copy = copy.deepcopy(board)
+		if solve(board_copy) > 1:
+			board[row][col] = temp
+		else:
+			removed += 1
+
 if __name__ == "__main__":
-	sudoku = gen_board()
-	for row in sudoku:
+	board = gen_board()
+	for row in board:
+		print(row)
+	print('\n')
+	puzzle = copy.deepcopy(board)
+	gen_puzzle(puzzle)
+	for row in puzzle:
 		print(row)
