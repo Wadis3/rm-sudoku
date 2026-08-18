9c4a56071c22c8b2e44be613e9638b9965d828fc
David Wadström <davidwadimp@gmail.com>
2026-08-18 09:06:02 +0000

    generate valid board

--- backend/sudoku.py
+++ backend/sudoku.py
@@ -2,36 +2,51 @@ import random
 
 def check(row, col, digit, board):
 	for i in range(9):
-		if col != i && board[row][i] == digit:
-			return false
-		if row != i && board[i][col] == digit:
-			return false
+		if col != i and board[row][i] == digit:
+			return False
+		if row != i and board[i][col] == digit:
+			return False
 	
-	for i in range(3 * (row / 3), 3 * ((row + 3) / 3)):
-		for j in range(3 * (col / 3), 3 * ((col + 3) / 3)):
-			if !(row == i && col == j) && board[i][j] == digit:
-				return false
+	for i in range(3 * (row // 3), 3 * ((row + 3) // 3)):
+		for j in range(3 * (col // 3), 3 * ((col + 3) // 3)):
+			if not(row == i and col == j) and board[i][j] == digit:
+				return False
 	
-	return true
+	return True
 
-def valid_at(row, col):
+def valid_at(row, col, board):
 	available = []
 	for digit in range(1, 10):
-		if check(row, col, digit):
-			available.add(digit)
+		if check(row, col, digit, board):
+			available.append(digit)
 	return available
 
 def gen_from(row, col, board):
 	if (col == 9):
-		col == 0
-		row = row + 1
-		if (row == 9):
-			return board
+		col = 0
+		row += 1
+		if row == 9:
+			return True
 	
-	random.choice(valid_at(row, col))
-	return gen_from(row + 1, col, board)
+	choices = valid_at(row, col, board)
+	random.shuffle(choices)
+	
+	for digit in choices:
+		board[row][col] = digit
+		
+		if gen_from(row, col + 1, board):
+			return True
+		
+		board[row][col] = 0
+	
+	return False
 
 def gen_board():
 	board = [[0 for _ in range(9)] for _ in range(9)]
-	
-	return gen_from(0, 0, board)
+	gen_from(0, 0, board)
+	return board
+
+if __name__ == "__main__":
+	sudoku = gen_board()
+	for row in sudoku:
+		print(row)
